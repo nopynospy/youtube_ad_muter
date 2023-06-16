2fa21a781cffb033be19ee675c1c74178bdb4781
nopynospy <tankelvin3310@gmail.com>
2023-06-16 14:22:10 +0800

    Add mute when ad text mutation

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# ad_muter
+# YouTube Ad Muter
+
+YouTube and its creators make money from ads, but some ads can be very loud and give viewers jump scares.
+
+This extension mutes ad videos (almost) as soon as they pop up.
+
+Now you can support your favourite creators without using ad-blocks while saving yourself from jumpscares.
+
+Note: this is still being tested.
+
+Original boilerplate taken from https://github.com/JasonXian/react-chrome-extension-boilerplate
+
+## Getting Started
+
+1. `npm i` to install dependancies
+2. `npm start` to start running the fast development mode Webpack build process that bundle files into the `dist` folder
+3. `npm i --save-dev <package_name>` to install new packages
+
+## Loading The Chrome Extension
+
+1. Open Chrome and navigate to `chrome://extensions/`
+2. Toggle on `Developer mode` in the top right corner
+3. Click `Load unpacked`
+4. Select the entire `dist` folder
+
+## Production Build
+
+1. `npm run build` to generate a minimized production build in the `dist` folder
+2. ZIP the entire `dist` folder (e.g. `dist.zip`)
+3. Publish the ZIP file on the Chrome Web Store Developer Dashboard!
+
+## Initial Steps
+
+1. `git init` to start a new git repo for tracking your changes, do an initial base commit with all the default files
+2. Update `package.json`, important fields include `author`, `version`, `name` and `description`
+3. Update `manifest.json`, important fields include `version`, `name` and `description`
+4. Update `webpack.commmon.js`, the title in the `getHtmlPlugins` function should be your extension name
+
+## Default Boilerplate Notes
+
+- Folders get flattened, static references to images from HTML do not need to be relative (i.e. `icon.png` instead of `../static/icon.png`)
+- Importing local ts/tsx/css files should be relative, since Webpack will build a dependancy graph using these paths
+- Update the manifest file as per usual for chrome related permissions, references to files in here should also be flattened and not be relative
+
+
+### Error SSL
+NODE_OPTIONS=--openssl-legacy-provider npm run start
+NODE_OPTIONS=--openssl-legacy-provider npm run build
--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,58 +1,21 @@
 
-function waitForElm(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
+function toggle_mute() {
+    const videos = document.getElementsByTagName("video");
+        for (let i = 0; i < videos.length; i++) {
+            videos[i].muted = true;
         }
-
-        const observer = new MutationObserver(mutations => {
-            console.log("mutation")
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-
-                // observer.disconnect();
-            }
-            
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true,
-            characterData: true
-        });
-    });
 }
 
 let observer = new MutationObserver(mutations => {
     for(let mutation of mutations) {
-        // console.log(mutation.target.nodeName)
-        // console.log(mutation)
         if (mutation.target.nodeName === "DIV") {
-            // console.log("DIV")
            const element = <HTMLElement>mutation.target
            const classList = Array.from(element.classList)
-           console.log(classList)
            if (classList.includes('ytp-ad-text')) {
-                console.log("AD playing!!!!!!")
+                // console.log("AD playing!!!!!!")
+                toggle_mute()
            }
-            // if (mutation.target === 'div#ad-text:d.ytp-ad-text') {
-
-            // }
         }
-        //  for(let addedNode of mutation.addedNodes) {
-        //      if (addedNode.nodeName === "ytd-action-companion-ad-renderer") {
-        //         console.log("AD")
-        //         console.log(mutation)
-        //         //  try{
-        //         //     let chats_dom = document.querySelector("div[aria-label='Search results.']"); 
-        //         //     chats_dom.setAttribute("style", "display: none;"); 
-        //         //     chats_dom.setAttribute("style", "pointer-events: none;");  
-                
-        //         //  } catch(err) {
-        //         //     console.log(err.message)
-        //         //  }
-        //       }
-        //   }
      }
  });
  observer.observe(document, { childList: true, subtree: true });
