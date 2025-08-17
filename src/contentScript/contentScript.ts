1185849ff0caa19e48c908f7722654011dd9f7aa
Kelvin Tan <85285558+nopynospy@users.noreply.github.com>
2025-08-17 16:29:06 +0800

    Update button mute detect

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -16,6 +16,19 @@ let observer = new MutationObserver(mutations => {
                 toggle_mute()
            }
         }
+        if (mutation.target.nodeName === "BUTTON") {
+            const element = <HTMLElement>mutation.target
+            const classList = Array.from(element.classList)
+            if (classList.includes('ytp-skip-ad-button')) {
+                console.log("Skip ad button found")
+                let divElement = document.getElementsByClassName("ytp-skip-ad-button")[0];
+                console.log(divElement)
+                divElement.addEventListener("click", function() {
+                    toggle_mute(false)
+                  });
+                }
+        }
+   
         for (const el of mutation.removedNodes) {
             const element = <HTMLElement>mutation.target
             const classList = Array.from(element.classList)
